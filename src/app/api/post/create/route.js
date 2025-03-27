3af2dffdfe8f93ae30b7c41a685d85a2a4e8fb13
Shubhang Gupta <2010311@kfintech.com>
2025-03-27 16:08:28 +0530

    slight change

--- src/app/api/post/create/route.js
+++ src/app/api/post/create/route.js
@@ -1,6 +1,7 @@
 import Post from "../../../../lib/models/post.model.js";
 import { connect } from "../../../../lib/mongodb/mongoose.js";
 import { currentUser } from "@clerk/nextjs/server";
+
 export const POST = async (req) => {
   const user = await currentUser();
   try {
